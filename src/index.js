488b87105df4741f640cd1d5ef62d88a08aa2931
Zachary Jones <zacharyjones13@gmail.com>
2025-07-20 22:17:02 -0400

    aa

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// ⬇️ Use HashRouter for Cloudflare compatibility
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>
+  </HashRouter>
 );
