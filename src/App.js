1c69aa6d1c29dbff8899cdd8c9624b94b0539dd0
Zachary Jones <zacharyjones13@gmail.com>
2025-07-20 22:17:37 -0400

    aa

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Quiz from './pages/Quiz';
 import Flashcards from './pages/Flashcards';
 
 function App() {
   return (
-    <Router>
+    <div>
+      <nav style={{ padding: '1rem', backgroundColor: '#f4f4f4' }}>
+        <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
+        <Link to="/quiz" style={{ marginRight: '1rem' }}>Quiz</Link>
+        <Link to="/flashcards">Flashcards</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/quiz" element={<Quiz />} />
         <Route path="/flashcards" element={<Flashcards />} />
       </Routes>
-    </Router>
+    </div>
   );
 }
 
